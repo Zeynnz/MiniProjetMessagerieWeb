0723a1f57c45365d53dc0f819bdcf9238bac2411
Zeynnz <zzeynn8@gmail.com>
2025-02-23 15:11:57 +0100

    html/css (pas finis)

---
+++ Vue/index.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Messagerie</title>
+
+</head>
+<body>
+    <div class="sidebar">
+        <h2>Conversations</h2>
+        <ul>
+            <li>Contact 1</li>
+            <li>Contact 2</li>
+            <li>Contact 3</li>
+        </ul>
+    </div>
+    <div class="chat-container">
+        <div class="messages">
+            <p><strong>Contact 1:</strong> Bonjour !</p>
+            <p><strong>Vous:</strong> Salut, comment ça va ?</p>
+        </div>
+        <div class="input-container">
+            <label>
+                <input type="text" placeholder="Ecrire un message...">
+            </label>
+            <button>Envoyer</button>
+        </div>
+    </div>
+</body>
+</html>
